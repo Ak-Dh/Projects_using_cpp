467eb962d28c0851e03cb49fe8cc4c64d685199a
Ak-Dh <akhilldj@gmail.com>
2025-05-13 13:08:56 +0200

    2

---
+++ cascading_of_operators.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Enter value of a : ";
+    cin>>a;
+    cout<<"Enter value of b : ";
+    cin>>b;
+    cout<<"the sum of "<<a<<" and "<<b<<" : "<<a+b;
+    cout<<"\nthe net of "<<a<<" and "<<b<<" : "<<a-b;
+    cout<<"\nthe product of "<<a<<" and "<<b<<" : "<<a*b;
+    cout<<"\nthe quotient of "<<a<<" divided by "<<b<<" : "<<a/b;
+    cout<<"\n";
+}
