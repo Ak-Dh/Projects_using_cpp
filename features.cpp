7db49f63b507234830a48536cb35a6127ed6aed6
Ak-Dh <akhilldj@gmail.com>
2025-05-13 12:49:13 +0200

    first push

---
+++ features.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"C++ is incremented version of C\n";
+    cout<<"Basic Concept of OOP\n";
+    cout<<"1.) Object\t\t2.) Classes\n";
+    cout<<"3.) Data Abstraction\t4.) Data Encapsulation\n";
+    cout<<"5.) Inheritence\t\t6.) Polymorphism\n";
+    cout<<"7.) Dynamic Binding\t8.) Message Passing\n";
+}
+
